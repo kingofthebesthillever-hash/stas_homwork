5b69b1cc8545dabc6058dc5413613cc26962a315
Стас <kingofthebesthillever@gmail.com>
2025-10-21 23:56:50 +0500

    4.2 добавили скорость черепахи, дорисовываем остальные цветы.

--- Chapter4/exersice_4.2.py
+++ Chapter4/exersice_4.2.py
@@ -2,6 +2,7 @@ import turtle
 import math
 
 bob = turtle.Turtle()
+bob.speed(10)
 
 def lines(t, n, length, angle):
     """Рисует черепашкой t последовательно n отрезков линии длинной length
@@ -25,7 +26,7 @@ def petal(t, r, angle):
     """Рисует лепесток с помощью t черепашки по радиусу r c углом angle """
     for i in range(2):
         arc(t, r, angle)
-        t.lt(180-angle)
+        t.lt(180 - angle)
 
 def flower(t, n, r, angle):
     """Рисует цветок с помощью t черепашки с
@@ -34,8 +35,21 @@ def flower(t, n, r, angle):
     angle: angle (degrees) that subtends the arcs"""
     for i in range(n):
         petal(t, r, angle)
-        t.lt(360.0/n)
+        t.lt(360.0 / n)
 
-flower(bob,7,60,60)
+
+flower(bob, 7, 60, 60) # рисуем первый цветок
+
+bob.penup()              # поднимаем и перемещаем перо
+bob.goto(140, 0)
+bob.pendown()
+
+flower(bob, 10, 45, 90)  # второй цветок с новыми параметрами
+
+bob.penup()             # поднимаем и перемещаем перо
+bob.goto(280, 0)
+bob.pendown()
+
+flower(bob, 20, 120, 25) # третий цветок с новыми параметрами
 
 turtle.exitonclick()
