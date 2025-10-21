b73ae34ade86a1cdd1d008bdea519cf3464e9376
Стас <kingofthebesthillever@gmail.com>
2025-10-22 01:22:16 +0500

    4.3 рисуем многоугольники из треугольников

--- Chapter4/exercise_4.3.py
+++ Chapter4/exercise_4.3.py
@@ -3,20 +3,37 @@ import turtle
 
 bob = turtle.Turtle()
 
+
 def draw_triangle(t, r, angle):
     """Рисуем равнобедренный треугольник с помощью черепашки Т:
     r: длинна бедра
     angle: радиус вершинного угла в градусах
     y: координата"""
 
-    y = r * math.sin(angle * math.pi / 180)
+    y = r * math.sin(angle * math.pi / 180)  # тригонометрическая функция для конвертации координат
 
     t.rt(angle)
     t.fd(r)
-    t.lt(90+angle)
-    t.fd(2*y)
-    t.lt(90+angle)
+    t.lt(90 + angle)
+    t.fd(2 * y)
+    t.lt(90 + angle)
     t.fd(r)
-    t.lt(180-angle)
+    t.lt(180 - angle)
 
-draw_triangle(bob, 50, 30)
+
+def draw_polygon(t, n, r):
+    """Рисуем многоугольник с помощью черепашки Т:
+    n: количество сегментов
+    r: длинна радиальных спиц"""
+    angle = 360.0 / n
+    for i in range(n):
+        draw_triangle(t, r, angle / 2)
+        t.lt(angle)
+    t.pu()                              # перемещаем перо на r * 2 + 10
+    t.fd(r * 2 + 10)
+    t.pd()
+
+
+draw_polygon(bob, 5, 30)
+draw_polygon(bob, 6, 30)
+draw_polygon(bob, 7, 30)
